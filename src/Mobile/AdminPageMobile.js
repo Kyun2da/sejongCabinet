5ff97b5549723ceeb0e2a9897912d6536eb56c63
Jongminfire <51112542+Jongminfire@users.noreply.github.com>
2020-12-31 16:26:21 +0900

    관리자페이지 PC 모바일 분리

--- src/Mobile/AdminPageMobile.js
+++ src/Mobile/AdminPageMobile.js
@@ -24,40 +24,38 @@ const Container = styled.div`
   width: 100%;
 `;
 
-const AdminpageTitle = styled.div`
+const MAdminpageTitle = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  font-size: 1.5vw;
+  font-size: 6vw;
   font-weight: bold;
-  padding: 1vh 0 2vh;
-  border-bottom: 1px solid RGB(200, 200, 200);
+  padding: 1vh 0 0.5vh;
 `;
 
 const useStyles = makeStyles(() => ({
-  cancleButton: {
+
+  McancleButton: {
     fontFamily: 'Noto Sans KR',
-    fontWeight: 'bold',
     backgroundColor: 'red',
-    color: 'white',
     opacity: 0.6,
-    padding: '0.5vh 2vw',
-    margin: '3vh 0 0',
-    borderRadius: '0.5vw',
+    fontSize: '0.5rem',
+    color: 'white',
+    borderRadius: '2vw',
     '&:hover': {
       backgroundColor: 'red',
       opacity: 1,
     },
   },
 
-  changeButton: {
+  MchangeButton: {
     fontFamily: 'Noto Sans KR',
     fontWeight: 'bold',
     backgroundColor: 'rgb(180,180,180)',
     color: 'white',
-    width: '25vw',
-    height: '6vh',
-    borderRadius: '0.5vw',
+    width: '60vw',
+    height: '5vh',
+    borderRadius: '3vw',
     margin: '1vh 0',
     '&:hover': {
       backgroundColor: 'rgb(150,150,150)',
@@ -65,7 +63,7 @@ const useStyles = makeStyles(() => ({
   },
 }));
 
-const AdminPage = (props) => {
+const AdminPageMobile = (props) => {
   const {
     onClickLogout,
     currentUserName,
@@ -105,181 +103,7 @@ const AdminPage = (props) => {
 
   return (
     <div style={{ width: '100%', height: '100%' }}>
-      <Default>
-        <header>
-          <div
-            style={{
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-              justifyContent: 'center',
-              backgroundColor: 'black',
-              position: 'absolute',
-              top: 0,
-              left: 0,
-              right: 0,
-              height: '10vh',
-              width: '100%',
-            }}
-          >
-            <div style={{ left: '2vw', position: 'absolute' }}>
-              <Link to="/main">
-                <img
-                  src={backwards}
-                  alt="backwards"
-                  style={{
-                    width: '1.5vw',
-                    filter: 'invert(100%)',
-                  }}
-                />
-              </Link>
-            </div>
-
-            <div
-              style={{
-                position: 'absolute',
-                right: '5vw',
-                backgroundColor: 'white',
-                borderRadius: '0.5rem',
-                padding: '0.5vh 1vw',
-                fontFamily: 'Noto Sans KR',
-              }}
-            >
-              {currentUserName}님 환영합니다!
-              <Button
-                onClick={handleClick}
-                style={{ backgroundColor: 'transparent', margin: '0 0 0 2vw' }}
-                disableRipple
-              >
-                <MenuIcon />
-              </Button>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorEl}
-                getContentAnchorEl={null}
-                anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
-                transformOrigin={{ vertical: 'top', horizontal: 'center' }}
-                open={Boolean(anchorEl)}
-                onClose={handleClose}
-              >
-                <MenuItem
-                  onClick={onClickLogout}
-                  style={{ fontFamily: 'Noto Sans KR' }}
-                >
-                  로그아웃
-                </MenuItem>
-              </Menu>
-            </div>
-          </div>
-        </header>
-        <Container style={{ marginTop: '14vh' }}>
-          <div
-            style={{
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-              border: '3px solid lightgray',
-              borderRadius: '2vw',
-              width: '40vw',
-              height: '75vh',
-              padding: '2vh 0',
-              columnGap: '2vh',
-            }}
-          >
-            <div
-              style={{
-                flexGrow: 1,
-                width: '80%',
-              }}
-            >
-              <AdminpageTitle style={{ fontFamily: 'Noto Sans KR' }}>
-                관리자 페이지
-              </AdminpageTitle>
-              <div
-                style={{
-                  height: 'auto',
-                  overflow: 'hidden',
-                }}
-              >
-                <div
-                  style={{
-                    justifyContent: 'space-evenly',
-                    alignItems: 'center',
-                    display: 'flex',
-                    margin: '4vh 0 2vh',
-                    fontSize: '2vw',
-                    flexDirection: 'column',
-                  }}
-                >
-                  <div style={{ fontFamily: 'Noto Sans KR' }}>
-                    현재 예약된 사물함 수 : {total}개
-                  </div>
-                  <center>
-                    <Button
-                      className={classes.cancleButton}
-                      onClick={toggleServer}
-                      style={{ fontFamily: 'Noto Sans KR' }}
-                    >
-                      {serverStatus?.status?.status ? '서버 닫기' : '서버 열기'}
-                    </Button>
-                  </center>
-                </div>
-              </div>
-            </div>
-            <div style={{ flexGrow: 2, width: '80%' }}>
-              <AdminpageTitle>비밀번호 변경</AdminpageTitle>
-              <div
-                style={{
-                  height: 'auto',
-                  overflow: 'hidden',
-                  margin: '2vh 0',
-                }}
-              >
-                <FormControl>
-                  <div
-                    style={{
-                      display: 'flex',
-                      justifyContent: 'center',
-                      alignItems: 'center',
-                      flexDirection: 'column',
-                      marginTop: '1vh',
-                      width: '100%',
-                    }}
-                  >
-                    <center>
-                      <TextField
-                        label="현재 비밀번호"
-                        variant="outlined"
-                        onChange={currnetPasswordHandler}
-                        style={{ width: '25vw', margin: '1.5vh 0' }}
-                      />
-                      <TextField
-                        label="변경 비밀번호"
-                        variant="outlined"
-                        onChange={changePasswordHandler}
-                        style={{ width: '25vw', margin: '1.5vh 0' }}
-                      />
-                      <TextField
-                        label="비밀번호 확인"
-                        variant="outlined"
-                        onChange={confirmPasswordHandler}
-                        style={{ width: '25vw', margin: '1.5vh 0' }}
-                      />
-                    </center>
-                    <Button
-                      className={classes.changeButton}
-                      onClick={passwordChangeFunc}
-                    >
-                      변경하기
-                    </Button>
-                  </div>
-                </FormControl>
-              </div>
-            </div>
-          </div>
-        </Container>
-      </Default>
-      {/* <Mobile>
+      <Mobile>
         <header>
           <div
             style={{
@@ -468,12 +292,12 @@ const AdminPage = (props) => {
             </div>
           </div>
         </Container>
-      </Mobile> */}
+      </Mobile>
     </div>
   );
 };
 
-AdminPage.propTypes = {
+AdminPageMobile.propTypes = {
   onClickLogout: PropTypes.func.isRequired,
   currentUserName: PropTypes.string.isRequired,
   updatePW: PropTypes.func.isRequired,
@@ -482,4 +306,4 @@ AdminPage.propTypes = {
   toggleServer: PropTypes.func.isRequired,
 };
 
-export default AdminPage;
+export default AdminPageMobile;
