2a7750a76530d2bac278c6cbe0ad4ac904c93880
Kyun Heo <kyun2dot@gmail.com>
2021-05-18 15:46:06 +0900

    initial commit

--- src/redux/cabinet/cabinet.types.js
+++
@@ -1,6 +0,0 @@
-const authTypes = {
-  // consts here
-  SET_CURRENT_CABINETS: 'SET_CURRENT_CABINETS',
-};
-
-export default authTypes;
--- src/redux/server/server.types.js
+++
@@ -1,6 +0,0 @@
-const serverTypes = {
-  // consts here
-  SET_CURRENT_SERVERS: 'SET_CURRENT_SERVERS',
-};
-
-export default serverTypes;
