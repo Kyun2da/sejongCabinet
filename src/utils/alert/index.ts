40bbb75f9695ecb8b7d36d4018fff5547df92cb4
Kyun Heo <kyun2dot@gmail.com>
2021-05-21 19:04:13 +0900

    update sweetalert2-react-content

--- src/utils/alert/index.ts
+++ src/utils/alert/index.ts
@@ -1,9 +1,12 @@
 import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
 
 type swalIconType = 'warning' | 'error' | 'success' | 'info' | 'question';
 
+const MySwal = withReactContent(Swal);
+
 const customSwal = (icon: swalIconType, title: string, text: string | null) => {
-  return Swal.fire({
+  return MySwal.fire({
     icon,
     title,
     text: text || '',
