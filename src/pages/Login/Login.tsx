5342e1cbede8eaeecae13a222b804451acfb0366
Kyun Heo <kyun2dot@gmail.com>
2021-05-21 19:04:36 +0900

    fix alert bug and input reset bug

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,14 +1,13 @@
-import React from 'react';
+import React, { useCallback, useEffect } from 'react';
 import { styled } from '@material-ui/core/styles';
 import Logo from '../../images/softwareLogo_origin.png';
 import media from '../../lib/styles/media';
 import { Button, Container, TextField } from '@material-ui/core';
 import { Redirect, useHistory } from 'react-router-dom';
-import { useForm } from 'react-hook-form';
+import { Controller, useForm } from 'react-hook-form';
 import FadeIn from 'react-fade-in';
 import useSignInWithEmailAndPassword from '../../hooks/useSignInWithEmailAndPassword';
 import { auth } from '../../config/firebase.config';
-import Swal from 'sweetalert2';
 import customSwal from '../../utils/alert';
 import getFirebaseErrorMessage from '../../utils/error/firebase';
 import { useAppDispatch } from '../../redux/hooks';
@@ -22,22 +21,7 @@ export type LoginInput = {
 };
 
 function Login({}: LoginProps) {
-  const {
-    register,
-    handleSubmit,
-    watch,
-    reset,
-    formState: { errors },
-  } = useForm<LoginInput>();
-
-  const onSubmit = async (data: LoginInput) => {
-    await signInwithEmailAndPassword(
-      `${data.studentID}@sejongCabinet.com`,
-      data.password,
-    );
-    reset({ studentID: '', password: '' });
-    console.log(user);
-  };
+  const { handleSubmit, control, reset, formState } = useForm<LoginInput>();
 
   const history = useHistory();
 
@@ -50,13 +34,27 @@ function Login({}: LoginProps) {
     dispatch(setUserUID(uid));
   };
 
-  if (error) {
+  const onSubmit = useCallback(async (data: LoginInput) => {
+    await signInwithEmailAndPassword(
+      `${data.studentID}@sejongCabinet.com`,
+      data.password,
+    );
+  }, []);
+
+  useEffect(() => {
+    if (!error) return;
     customSwal(
       'error',
       '로그인 에러입니다.',
-      getFirebaseErrorMessage(error.code),
+      getFirebaseErrorMessage(error?.code),
     );
-  }
+  }, [error]);
+
+  useEffect(() => {
+    if (formState.isSubmitSuccessful) {
+      reset({ studentID: '', password: '' });
+    }
+  }, [formState, reset]);
 
   if (loading) {
     return <div>로딩중...</div>;
@@ -74,26 +72,40 @@ function Login({}: LoginProps) {
         <LogoTitle>SEJONG UNIV</LogoTitle>
         <LogoTitle2>소프트웨어학과 사물함</LogoTitle2>
         <LoginForm onSubmit={handleSubmit(onSubmit)}>
-          <LoginTextField
-            label="학번"
-            variant="outlined"
-            {...register('studentID', {
-              required: true,
-              // minLength: 8,
-              // maxLength: 8,
-            })}
-            // inputProps={{ maxLength: 8 }}
-            helperText={errors.studentID && '학번 8자리를 입력해주세요.'}
+          <Controller
+            name="studentID"
+            control={control}
+            defaultValue=""
+            rules={{ required: true, minLength: 8, maxLength: 8 }}
+            render={({ field: { onChange, value }, fieldState: { error } }) => (
+              <LoginTextField
+                label="학번"
+                variant="outlined"
+                onChange={onChange}
+                value={value}
+                error={!!error}
+                helperText={error ? '학번 8자리를 입력해주세요.' : null}
+              />
+            )}
           />
-          <LoginTextField
-            label="비밀번호"
-            variant="outlined"
-            type="password"
-            {...register('password', { required: true, minLength: 6 })}
-            inputProps={{ maxLength: 12 }}
-            helperText={
-              errors.password && '6글자 이상의 패스워드를 입력해주세요.'
-            }
+          <Controller
+            name="password"
+            control={control}
+            defaultValue=""
+            rules={{ required: true, minLength: 6 }}
+            render={({ field: { onChange, value }, fieldState: { error } }) => (
+              <LoginTextField
+                label="비밀번호"
+                variant="outlined"
+                type="password"
+                value={value}
+                onChange={onChange}
+                error={!!error}
+                helperText={
+                  error ? '6글자 이상의 패스워드를 입력해주세요.' : null
+                }
+              />
+            )}
           />
           <LoginButton type="submit" variant="contained">
             로그인
