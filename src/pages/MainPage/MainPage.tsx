a5d77848b49e240984dad9c5dfa6b2f7b81f6199
Kyun Heo <kyun2dot@gmail.com>
2021-05-26 00:16:42 +0900

    메인페이지 메뉴추가

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -2,43 +2,38 @@ import React, { useState } from 'react';
 import Header from '../../Components/Header';
 import HelperButton from '../../Components/HelperButton';
 import HelperModal from '../../Components/HelperModal';
-import { useAppSelector } from '../../redux/hooks';
-import { Redirect, useHistory } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
+import MenuInfo from '../../Components/MenuInfo';
+import { useObject } from '../../hooks/useObject';
+import { auth, database } from '../../config/firebase.config';
+import useAuthState from '../../hooks/useAuthState';
 
 export type MainPageProps = {};
 
 function MainPage({}: MainPageProps) {
-  const uuid = useAppSelector((state) => state.users.uuid);
-
-  // mainpage -> userpage 테스트용으로 추가했습니다 (21-05-22)
-  const history = useHistory();
+  const [user, authLoading, authError] = useAuthState(auth);
+  const [userInfo, userInfoLoading, userInfoError] = useObject(
+    database.ref(`users/${user?.uid}`),
+  );
   const [openModal, setOpenModal] = useState<boolean>(false);
   const handleOpen = () => {
     setOpenModal(!openModal);
   };
 
+  if (authLoading || userInfoLoading) {
+    return <div>로딩중...</div>;
+  }
+
+  if (!user) {
+    return <Redirect to="/login" />;
+  }
+
   return (
     <>
       <Header>
         <HelperButton onClick={handleOpen} />
+        <MenuInfo name={userInfo?.val().name} />
       </Header>
-      {uuid}
-
-      {/* mainpage -> userpage 테스트용으로 추가했습니다 (21-05-22) */}
-      <div>
-        <button
-          style={{ position: 'absolute', top: '50%', right: '40%' }}
-          onClick={() => history.push('/userpage')}
-        >
-          to UserPage
-        </button>
-        <button
-          style={{ position: 'absolute', top: '50%', left: '40%' }}
-          onClick={() => history.push('/adminpage')}
-        >
-          to AdminPage
-        </button>
-      </div>
       <HelperModal open={openModal} setOpen={handleOpen} />
     </>
   );
