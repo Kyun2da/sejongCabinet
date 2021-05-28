bf7594144c442fd32f7f6441a75aeb77b72f5bd3
Kyun Heo <kyun2dot@gmail.com>
2021-05-28 22:08:55 +0900

    feat : backButton 컴포넌트 개발

---
+++ src/Components/BackButton/BackButton.tsx
@@ -0,0 +1,25 @@
+import { styled } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+import backwards from '../../images/Backward.png';
+
+export default function BackButton() {
+  return (
+    <BackwardsContainer>
+      <Link to="/main">
+        <img
+          src={backwards}
+          alt="backwards"
+          style={{
+            width: '1.5vw',
+            filter: 'invert(100%)',
+          }}
+        />
+      </Link>
+    </BackwardsContainer>
+  );
+}
+
+const BackwardsContainer = styled('div')({
+  left: '2vw',
+  position: 'absolute',
+});
