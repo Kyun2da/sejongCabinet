964eac107cea165bfbb774bfb47032856e1a671f
Kyun Heo <kyun2dot@gmail.com>
2021-06-01 17:24:10 +0900

    fix : 사물함 예약 취소, 유저 예약 정보만초기화하지말고 사물함도 같이 초기화

--- src/utils/firebase/changeFirebaseCancelCabinetUser.ts
+++ src/utils/firebase/changeFirebaseCancelCabinetUser.ts
@@ -1,11 +1,16 @@
 import { database } from '../../config/firebase.config';
 import { serverStatusType } from '../../redux/server/serverSlice';
 
-const changeFirebaseCancelCabinetUser = (uuid: string) => {
+const changeFirebaseCancelCabinetUser = (
+  cabinetNum: number,
+  index: number,
+  uuid: string,
+) => {
   const postData = {
     cabinetIdx: 0,
     cabinetTitle: 0,
   };
+  database.ref(`/cabinet/${cabinetNum}/item/${index}`).set({ status: 0 });
   return database.ref(`/users/${uuid}`).update(postData);
 };
 
